48cbd914d8641b9adc7f8e8df37d301d8a94df58
kamilPlewka <kamilplewka12a@wp.pl>
2022-07-12 22:34:52 +0200

    now user can sort by title

--- Pages/Library/library-page.js
+++ Pages/Library/library-page.js
@@ -1,7 +1,11 @@
 import { createArticle } from "../global-components/createArticle.js";
 import { checkWhatBtnIsInLibrary } from "../global-components/btnAddToLibrary/checkIfBtnIsInLibrary.js";
 import { sortByPublished } from "./components/sortByPublished.js";
-
+import { sortByTitle } from "./components/sortByTitle.js";
+const btnNavigateMain = document.querySelector(
+  ".container-btn-sort-navigateMain"
+);
+const btnSortByTitle = document.querySelector(".container-btn-sort-byTitle");
 const btnSortbyPublished = document.querySelector(
   ".container-btn-sort-byPublished"
 );
@@ -12,5 +16,8 @@ window.onload = function () {
     createArticle(JSON.parse(article));
   });
 };
-
+btnSortByTitle.addEventListener("click", sortByTitle);
 btnSortbyPublished.addEventListener("click", sortByPublished);
+btnNavigateMain.addEventListener("click", () => {
+  location.href = "../Main-page/index.html";
+});
