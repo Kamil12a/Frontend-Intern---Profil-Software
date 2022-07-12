f4fac1bfd64a3490b8d8ab5f84ae9af38b9159cd
kamilPlewka <kamilplewka12a@wp.pl>
2022-07-12 14:27:48 +0200

    add styles to articles container

--- Pages/Main-page/components/createArticle.js
+++ Pages/Main-page/components/createArticle.js
@@ -1,11 +1,46 @@
 const container_articles = document.querySelector(".container-articles");
-export function createArticle(articlesData){
-    articlesData.forEach(element => {
-        console.log(element)
-        let p = document.createElement("p");
-        p.innerText="xdxd"
-        container_articles.appendChild(p)
-    });
+export function createArticle(articlesData) {
+  articlesData.forEach((single_Article) => {
+    let container_articles_single_article = document.createElement("div");
+    let title = document.createElement("h4");
+    let newsSite = document.createElement("a");
+    let publishedAt = document.createElement("p");
+    let summary = document.createElement("p");
+    let buttonAddLibrary = document.createElement("button");
+    let container_articles_single_article_btn_box =
+      document.createElement("div");
+
+    title.innerText = single_Article.title;
+    newsSite.href = single_Article.url;
+    newsSite.innerHTML = "News Site: " + single_Article.newsSite;
+    publishedAt.innerHTML = single_Article.publishedAt;
+    summary.innerHTML = single_Article.summary;
+    buttonAddLibrary.innerHTML = "add to library";
+
+    container_articles_single_article.classList.add(
+      "container_articles_single_article"
+    );
+    title.classList.add("container_articles_single_article_title");
+    newsSite.classList.add("container_articles_single_article_newsSite");
+    publishedAt.classList.add("container_articles_single_article_publishedAt");
+    summary.classList.add("container_articles_single_article_summary");
+    container_articles_single_article_btn_box.classList.add(
+      "container_articles_single_article_btn_box"
+    );
+    buttonAddLibrary.classList.add(
+      "container_articles_single_article_btn_box_addLibrary"
+    );
+
+    container_articles_single_article_btn_box.appendChild(buttonAddLibrary);
+
+    container_articles_single_article.appendChild(title);
+    container_articles_single_article.appendChild(newsSite);
+    container_articles_single_article.appendChild(publishedAt);
+    container_articles_single_article.appendChild(summary);
+    container_articles_single_article.appendChild(
+      container_articles_single_article_btn_box
+    );
+
+    container_articles.appendChild(container_articles_single_article);
+  });
 }
-  
-  
